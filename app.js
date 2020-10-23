47a0ffc41cc3e04eec516c720661e38475443398
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-10-23 15:31:37 +0200

    Adding JS to slide between images/text using buttons

---
+++ app.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+
+let texts = document.querySelectorAll(".text");
+let images = document.querySelectorAll(".people-img");
+let leftBtn = document.querySelector("#left-button");
+let rightBtn = document.querySelector("#right-button");
+
+leftBtn.addEventListener("click", () => {
+  showDivs((slideIndex -= 1));
+});
+rightBtn.addEventListener("click", () => {
+  showDivs((slideIndex += 1));
+});
+
+function showDivs(n) {
+  if (n > texts.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = texts.length;
+  }
+  for (let i = 0; i < texts.length; i++) {
+    texts[i].classList.add("inactive");
+    images[i].classList.add("inactive");
+  }
+  texts[slideIndex - 1].classList.remove("inactive");
+  images[slideIndex - 1].classList.remove("inactive");
+  console.log(slideIndex);
+}
+
+//Call
+// showDivs(slideIndex);
